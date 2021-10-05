76e7127a1015f151fdd4962f8c33c886b670bd0a
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-05 16:40:56 +0530

    Map component

--- client/src/components/MapComponent/index.js
+++ client/src/components/MapComponent/index.js
@@ -1,6 +1,6 @@
 
 import {useState} from "react"
-import { MapContainer, TileLayer, Marker, Tooltip, useMapEvents,Popup} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip,MapConsumer,LayerGroup,Circle, useMapEvents,Popup} from "react-leaflet";
 
 export function LocationMarker() {
    
@@ -11,16 +11,33 @@ export function LocationMarker() {
             map.locate()
         },
         locationfound(e){
-            console.log(e)
+            fetch(`https://us1.locationiq.com/v1/reverse.php?key=pk.2a3e1f2f2006da3635308c25d2ea7215&lat=${e.latlng.lat}&lon=${e.latlng.lng}&format=json`)
+  .then(response => response.json())
+  .then(data => console.log(data));
+           
             setPosition(e.latlng)
             map.flyTo(e.latlng,map.getZoom())
+
         },
+    })
+    map.on('click',function(e){
+      if(position !== null) console.log(e.latlng.lat-position.lat)
     })
 
     return position === null ? null :(
+        <>
         <Marker position={position}>
             <Popup>Your current Location</Popup>
         </Marker>
+        <LayerGroup>
+        <Circle
+          center={position}
+          pathOptions={{ color: 'green', fillColor: 'green' }}
+          radius={3000}
+        />
+        
+      </LayerGroup>
+      </>
     )
 }
 
@@ -30,13 +47,13 @@ const MapComponent = () => {
     <div className="">
     <MapContainer
     center={{ lat: 51.505, lng: -0.09 }}
-    zoom={13}
+    zoom={15}
     scrollWheelZoom={false}>
     <TileLayer
       attribution='<a href="http://osm.org/copyright"></a>'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    
+     
     <LocationMarker />
   </MapContainer>
     </div>
