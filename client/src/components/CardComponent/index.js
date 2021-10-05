3fea55eaa43b6bd406ef9d9f983418291522c111
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-05 16:42:27 +0530

    Card Compoentn

---
+++ client/src/components/CardComponent/index.js
@@ -0,0 +1,16 @@
+import "./style.css"
+
+const CardComponent = () => {
+    return <>
+        <div className="card mt-4 mb-4 mx-2">
+            <div className="card--img--wrapper">
+
+            </div>
+            <div className="card--content--wrapper">
+
+            </div>
+        </div>
+    </>
+}
+
+export default CardComponent;
