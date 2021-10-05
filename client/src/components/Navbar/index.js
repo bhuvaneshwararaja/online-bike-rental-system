df70c1c099f0a1874440247ce8fc997eda9a1af3
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-05 16:41:09 +0530

    fixing UI

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,21 +2,15 @@ import {useState} from "react"
 import "./style.css"
 const NavBar = () => {
     return <>
-       <nav className="flex justify-between">
-          <a href="#" >Logo</a>
+       <nav className="flex justify-between items-center">
+          <a href="/" className="logo--wrapper flex items-center">
+          <img src="./assest/images/bike-logo.png" alt=""></img>
+          <h1 className="mt-9">BikeForAll</h1>
+          </a>
           <ul class="flex">
+           
             <li class="px-4 py-1 text-xl">
-              <a href="#">
-                Home
-              </a>
-            </li>
-            <li class="px-4 py-1 text-xl">
-              <a href="#">
-                About
-              </a>
-            </li>
-            <li class="px-4 py-1 text-xl">
-              <a href="#">
+              <a href="/" className="btn--c">
                 Sign-In/Register
               </a>
             </li>
