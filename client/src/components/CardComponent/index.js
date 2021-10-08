191bd2ff795953eec050229517c22e7321733394
KousiKrishnan <kousikrishnan19501@gmail.com>
2021-10-08 16:31:51 +0530

    update card component

--- client/src/components/CardComponent/index.js
+++ client/src/components/CardComponent/index.js
@@ -4,10 +4,15 @@ const CardComponent = () => {
     return <>
         <div className="card mt-4 mb-4 mx-2">
             <div className="card--img--wrapper">
-
+                <img src="./assest/images/royalenfield.png" ></img>
             </div>
             <div className="card--content--wrapper">
-
+                <h1>Royal Enfiled classic 350</h1>
+                <div>
+                    <p>Bike No:TN76AH1255</p>
+                    <p>OwnerName:Ishaq</p>
+                    <p>Contact:7686534278</p>
+                </div>
             </div>
         </div>
     </>
