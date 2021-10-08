3a5de71002ae62f877a7012de7af7646d86fea4a
KousiKrishnan <kousikrishnan19501@gmail.com>
2021-10-08 16:32:44 +0530

    update nav component

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,18 +2,35 @@ import {useState} from "react"
 import "./style.css"
 const NavBar = () => {
     return <>
-       <nav className="flex justify-between items-center">
+       <nav className="flex justify-between items-center nav--bar">
           <a href="/" className="logo--wrapper flex items-center">
           <img src="./assest/images/bike-logo.png" alt=""></img>
-          <h1 className="mt-9">BikeForAll</h1>
+          {/* <h1 className="mt-9">BikeForAll</h1> */}
           </a>
-          <ul class="flex">
-           
-            <li class="px-4 py-1 text-xl">
-              <a href="/" className="btn--c">
-                Sign-In/Register
-              </a>
-            </li>
+          <ul class="flex nav--links">
+           <li className="px-4 py-2 ">
+             <a href="#">Home</a>
+           </li>
+           <li className="px-4 py-2 ">
+             <a href="#">SearchBikes</a>
+    
+           </li>
+           <li className="px-4 py-2 ">
+             <a href="#">SwitchRenter</a>
+            
+
+           </li>
+           <li className="px-4 py-2 ">
+             <a href="#">About</a>
+            
+
+           </li>
+           <li className="px-4 py-2">
+             <a href="#">Logout</a>
+            
+
+           </li>
+            
           </ul>
        </nav>
     </>
