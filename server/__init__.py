372f5b6af2cb4be2860659d6e330b255660cbe67
KousiKrishnan <kousikrishnan19501@gmail.com>
2021-10-17 16:36:53 +0530

    intizialize route

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_restful import Api
+# from admin import Admin
+from user import User
 
+# Flask app creation
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def index():
-    return "server runs..."
+
+# User route functions,
+app.add_url_rule('/user/signup/', view_func=User.sign_up, methods=['POST'])
+
+# Python main function
+if __name__ == '__main__':
+    app.run(debug=True)
