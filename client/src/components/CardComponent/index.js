f49488fd3866dd37cec64063d15654306a206209
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-17 16:19:24 +0530

    changing card UI

--- client/src/components/CardComponent/index.js
+++ client/src/components/CardComponent/index.js
@@ -2,9 +2,9 @@ import "./style.css"
 
 const CardComponent = () => {
     return <>
-        <div className="card mt-4 mb-4 mx-2">
-            <div className="card--img--wrapper">
-                <img src="./assest/images/royalenfield.png" ></img>
+        <div className="card mt-4 mb-4 mx-2 flex justify-around">
+            <div className="card--img--wrapper" style={{"backgroundImage":`url(${"./assest/images/royalenfield.png"})`}}>
+                {/* <img src="./assest/images/royalenfield.png" ></img> */}
             </div>
             <div className="card--content--wrapper">
                 <h1>Royal Enfiled classic 350</h1>
@@ -13,6 +13,9 @@ const CardComponent = () => {
                     <p>OwnerName:Ishaq</p>
                     <p>Contact:7686534278</p>
                 </div>
+            </div>
+            <div className="flex flex-col justify-center">
+                <button className="btn--c">TakeBike</button>
             </div>
         </div>
     </>
