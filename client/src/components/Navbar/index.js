effa5c33b91f5800651de233e0d3bc4b966adb0a
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-19 14:59:10 +0530

    nav component

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,28 +5,27 @@ const NavBar = () => {
        <nav className="flex justify-between items-center nav--bar">
           <a href="/" className="logo--wrapper flex items-center">
           <img src="./assest/images/bike-logo.png" alt=""></img>
-          {/* <h1 className="mt-9">BikeForAll</h1> */}
           </a>
           <ul class="flex nav--links">
-           <li className="px-4 py-2 ">
+           <li className="px-4 py-2 link">
              <a href="#">Home</a>
            </li>
-           <li className="px-4 py-2 ">
-             <a href="#">SearchBikes</a>
+           <li className="px-4 py-2 link">
+             <a href="#">TakeBike</a>
     
            </li>
-           <li className="px-4 py-2 ">
-             <a href="#">SwitchRenter</a>
+           <li className="px-4 py-2 link">
+             <a href="#">PostBike</a>
             
 
            </li>
-           <li className="px-4 py-2 ">
+           <li className="px-4 py-2 link">
              <a href="#">About</a>
             
 
            </li>
-           <li className="px-4 py-2 ">
-             <a href="#">Logout</a>
+           <li className="px-4 py-2 link ">
+             <a href="#" className="logout">Logout</a>
             
 
            </li>
