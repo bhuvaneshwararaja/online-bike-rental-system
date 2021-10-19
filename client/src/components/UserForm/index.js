ecb13c6403e1302d0a70277a3f52c74016c020d6
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-19 14:59:31 +0530

    reg component

--- client/src/components/UserForm/index.js
+++ client/src/components/UserForm/index.js
@@ -6,6 +6,15 @@ import {  checkpass,
     checkmail,
     checkmobile} from "../../Helper/validate"
 const UserForm = () => {
+    const error = {
+        nameError:"",
+        mailError:"",
+        passwordError:"",
+        mismatch:"",
+        mobileError:""
+    }
+    const [authError,setauthError] = useState(error)
+    console.log(authError)
     const regUser = {
         firstName:"",
         email:"",
@@ -40,13 +49,75 @@ const UserForm = () => {
         })
         console.log(user)
     }
-    function Registration(e){
-        const {name,value} = e.target
+    function saveState(name,value){
         setNewUser({
             ...newUser,
             [name]:value
         })
         console.log(newUser)
+    }
+    function Registration(e){
+        const {name,value} = e.target
+        if(e.target.name === "firstName"){
+           let check = checkusername(e.target.value);
+           if(check == true){
+               setauthError(error)
+               saveState(name,value)
+           }
+           else{
+               setauthError({...error,nameError: check})
+               console.log(authError)
+           }
+
+        }
+        else if(e.target.name === "email"){
+            let check = checkmail(e.target.value);
+            if(check == true){
+                setauthError(error)
+                saveState(name,value)
+            }
+            else{
+                setauthError({...error,mailError: check})
+                console.log(authError)
+            }
+ 
+         }
+         else if(e.target.name === "password"){
+            let check = checkpassword(e.target.value);
+            if(check == true){
+                setauthError(error)
+                saveState(name,value)
+            }
+            else{
+                setauthError({...error,passwordError: check})
+                console.log(authError)
+            }
+ 
+         }
+         else if(e.target.name === "confirmPassword"){
+            let check = checkpass(e.target.value,newUser.password);
+            if(check == true){
+                setauthError(error)
+            }
+            else{
+                setauthError({...error,mismatch: check})
+                console.log(authError)
+            }
+ 
+         }
+         else if(e.target.name === "mobileNo"){
+            let check = checkmobile(e.target.value);
+            if(check == true){
+                setauthError(error)
+                saveState(name,value)
+            }
+            else{
+                setauthError({...error,mobileError: check})
+                console.log(authError)
+            }
+ 
+         }
+     
     }
 
     function HandleUserSubmit(e){
@@ -78,27 +149,41 @@ const UserForm = () => {
                             <tbody>
                                 <tr>
                                     <td><label>First Name</label></td>
-                                    <td><input type="text" className="form--inp" name="firstName" onChange={Registration}></input></td>
+                                    <td className="flex flex-col"><input type="text" className="form--inp" name="firstName" onChange={Registration}></input>
+                                    <p style={{color:"crimson","width":"100%"}}>{authError.nameError != "" ? authError.nameError : ""}</p>
+                                    </td>
 
                                 </tr>
                                 <tr>
                                     <td><label>Email</label></td>
-                                    <td><input type="email" className="form--inp" name="email" onChange={Registration}></input></td>
+                                    <td className="flex flex-col"><input type="email" className="form--inp" name="email" onChange={Registration}></input>
+                                    <p style={{color:"crimson","width":"100%"}}>{authError.mailError != "" ? authError.mailError : ""}</p>
+                                    
+                                    </td>
 
                                 </tr>
                                 <tr>
                                     <td><label>Password</label></td>
-                                    <td><input type="password" className="form--inp" name="password" onChange={Registration}></input></td>
+                                    <td className="flex flex-col"><input type="password" className="form--inp" name="password" onChange={Registration}></input>
+                                    <p style={{color:"crimson","width":"100%"}}>{authError.passwordError != "" ? authError.passwordError : ""}</p>
+                                    
+                                    </td>
 
                                 </tr>
                                 <tr>
                                     <td><label>Confirm-Password</label></td>
-                                    <td><input type="password" className="form--inp" name="confirmPassword" onChange={Registration}></input></td>
+                                    <td className="flex flex-col"><input type="password" className="form--inp" name="confirmPassword" onChange={Registration}></input>
+                                    <p style={{color:"crimson","width":"100%"}}>{authError.mismatch != "" ? authError.mismatch : ""}</p>
+                                    
+                                    </td>
 
                                 </tr>
                                 <tr>
                                     <td><label>Mobile-No</label></td>
-                                    <td><input type="text" className="form--inp" name="mobileNo" onChange={Registration}></input></td>
+                                    <td className="flex flex-col"><input type="text" className="form--inp" name="mobileNo" onChange={Registration}></input>
+                                    <p style={{color:"crimson","width":"100%"}}>{authError.mobileError != "" ? authError.mobileError : ""}</p>
+                                    
+                                    </td>
 
                                 </tr>
                                 </tbody>
