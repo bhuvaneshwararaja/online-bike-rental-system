97113e0fa63934f4da26a321a5bf09edfc0d3c58
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-20 15:25:23 +0530

    insertion

--- server/bike.py
+++ server/bike.py
@@ -1,3 +1,16 @@
 import flask
 from flask import jsonify
 from bike_mongo import BikeMongo
+
+class Bike:
+    def __init__(self):
+        pass
+    @staticmethod
+    def Insert_Bike_Details():
+        try:
+            bikeDetails = flask.request.json['postDetails']
+        except:
+            return jsonify({"ReplyCode": "0", "ReplyMessage": "Error in json object receive during bike details"})
+        bike_mongo.add_bike(bikeDetails)
+        return jsonify({"ReplyCode": "1", "ReplyMessage": "Insertion successful"})
+
