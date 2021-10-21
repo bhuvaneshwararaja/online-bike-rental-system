b9afb4c41631f1e003aedda50e20f5dda7ceb806
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-21 19:00:36 +0530

    changing layout

--- client/src/components/CardComponent/index.js
+++ client/src/components/CardComponent/index.js
@@ -1,24 +1,42 @@
 import "./style.css"
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const CardComponent = () => {
     return <>
-        <div className="card mt-4 mb-4 mx-2 flex justify-around">
-            <div className="card--img--wrapper" style={{"backgroundImage":`url(${"./assest/images/royalenfield.png"})`}}>
-                {/* <img src="./assest/images/royalenfield.png" ></img> */}
+        <div className="card mt-4 mb-4 mx-2 flex justify-between">
+            
+            <div className="w-1/2 relative overlay--helper">
+            <div className="card--img--wrapper" >
+            <img src="./assest/images/royalenfield.jpg"></img>
+            </div>
+            <div className="absolute img--overlay flex flex-col justify-center items-center">
+                <h1 className="text-white text-2xl">RS.300/hr</h1>
+                <button className="btn--c">Enquire</button>
+                <button className="btn--c">Route</button>
+            </div>
             </div>
             <div className="card--content--wrapper">
-                <h1>Royal Enfiled classic 350</h1>
+                <h1 className="uppercase">Royal Enfiled classic 350</h1>
+                <p className="flex items-center text-gray-500"><FaMapMarkerAlt className="mr-1"/> mepco schlenk , mathiyasennai </p>
                 <div>
-                    <p>Bike No:TN76AH1255</p>
-                    <p>OwnerName:Ishaq</p>
-                    <p>Contact:7686534278</p>
+                    <table>
+                        <tr>
+                            <td className="pd-p5 bld uppercase">OwnerName:</td>
+                            <td  className="pd-p5">Ishaq</td>
+                        </tr>
+                        <tr>
+                            <td  className="pd-p5 bld uppercase">BikeNo:</td>
+                            <td  className="pd-p5">TN123ED</td>
+                        </tr>
+                        <tr>
+                            <td  className="pd-p5 bld uppercase">BikeName:</td>
+                            <td  className="pd-p5">Royal Enfield</td>
+                        </tr>
+                    </table>
+                   
+
                 </div>
-                <div className="flex flex-col justify-center">
-                <button className="btn--c">TakeBike</button>
-            </div>
-            <div className="flex flex-col justify-center">
-                <button className="btn--c">Enquire</button>
-            </div>
+                
             </div>
             
         </div>
