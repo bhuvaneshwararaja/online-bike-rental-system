2b67618abac81da1432b461ccc6513c965e244dd
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-10-21 19:04:47 +0530

    update home layout

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -9,8 +9,11 @@ const BikeRentalHome = () => {
    console.log(getAddress)
     return <>
         <NavBar/>
-       <div className="flex items-center justify-center " style={{"min-height": "90vh"}}>
+       {/* <div className="flex items-center justify-center " >
        <div className="map--wrapper flex">
+       <div className="w-1/2">
+            <MapComponent setaddress={setAddress}/>
+        </div>
         <div className="w-1/2">
         <div className="cards--section">
             <div>
@@ -35,19 +38,24 @@ const BikeRentalHome = () => {
             </div>
             </div>
         </div>
-        <div className="w-1/2">
-            <MapComponent setaddress={setAddress}/>
-        </div>
+        
 </div>
-       </div>
-        {/* <div className="flex">
-        <div className="w-1/2 ">
-        <div className="cards--section">
-            <div>
+       </div> */}
+        <div className="flex">
+        <div className="w-1/2">
+        <MapComponent setaddress={setAddress}/>
+
+        </div>
+        <div className="w-1/2">
+        <div className="loc--section flex flex-col justify-center items-center">
             <input type="text" className="address--inp" value={getAddress !== undefined ? getAddress:""} placeholder="current location"></input>
             <input type="text" className="address--inp"  placeholder="Destination"></input>
+        </div>
+        <div className="cards--section">
+            <div>
 
-            {getAddress !== undefined ? <div className="scroll" style={{"width": "inherit","height": "100vh","overflowY":"auto"}}>
+
+            {getAddress === undefined ? <div className="scroll flex flex-col" style={{"width": "inherit","height": "100vh","overflowY":"auto"}}>
                 <CardComponent/>
                 <CardComponent/>
                 <CardComponent/>
@@ -66,12 +74,9 @@ const BikeRentalHome = () => {
             </div>
         </div>
         </div>
-        <div className="w-1/2">
-        <MapComponent setaddress={setAddress}/>
+       
 
         </div>
-
-        </div> */}
        
     </>
 }
